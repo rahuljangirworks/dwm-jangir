c8c1e2db70ccf49a0d1da141d27477803a97488f
Rahul Jangir <contact@rahuljangir.work>
2026-08-30 10:27:20 +0530

    fix(lightdm): use dwm-jangir as default session

--- tests/test-lightdm-config.sh
+++ tests/test-lightdm-config.sh
@@ -23,7 +23,7 @@ logind-check-graphical=true
 
 [Seat:*]
 greeter-session=slick-greeter
-user-session=dwm
+user-session=dwm-jangir
 CONF
 cmp -s "$work/fedora.expected" "$fedora_stage/etc/lightdm/lightdm.conf"
 cmp -s "$repo/lightdm/lightdm.conf" "$fedora_stage/etc/lightdm/lightdm.conf"
